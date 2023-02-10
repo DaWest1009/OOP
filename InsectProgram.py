bfe42db800d7ef8669777528aa638b5045117aa5
tasi <daunte_west1@baylor.edu>
2023-02-09 21:43:36 -0600

    finished lecture exercise

--- InsectProgram.py
+++ InsectProgram.py
@@ -3,15 +3,19 @@ import InsectClass as i
 
 # The main function.
 def main():
+
     insect = i.Insect()
 
+    sel = ''
+    while sel != 'x':
+        print('\nThis fly can fly.... (press any button to see)' )
+        insect.miles_flown()
+        
 
-    print('\nThis fly can fly.... (press any button to see)' )
-    insect.miles_flown()
-    input()
+        print(insect.get_length_of_flight(), "mile(s)")
+        sel = input("Press any button to repeat, or x to exit")
 
-    print(insect.get_length_of_flight(), "mile(s)")
-    
             
 # Call the main function.
 main()
+
